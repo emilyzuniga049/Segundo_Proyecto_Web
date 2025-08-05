ff46e09ad98b3c6228db3f15000314551df62bc3
emilyzuniga049 <emilyzuniga049@gmail.com>
2025-08-04 23:15:44 -1200

    Agrego js con funcion para registrar usuario

---
+++ js/user_registration.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Ensure the DOM is fully loaded before attaching event listeners
+  document.getElementById('register-Form')
+    .addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    storeInputs(); // Call the function to store inputs
+  });
+});
+
+function storeInputs() {
+  const name = document.getElementById('first-name').value;
+  const lastname = document.getElementById('last-name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const password2 = document.getElementById('repeat-password').value;
+  const address = document.getElementById('address').value;
+  const country = document.getElementById('country').value;
+  const state = document.getElementById('state').value;
+  const city = document.getElementById('city').value;
+  const phone = document.getElementById('phone').value;
+  if (password === password2) {
+    //save to localstorage as a JSON object
+    const userData = {
+      name: name,
+      lastname: lastname,
+      address: address,
+      country: country,
+      state: state,
+      city: city,
+      phone: phone,
+      email: email,
+      password: password,
+    }
+
+    let users = JSON.parse(localStorage.getItem('users'));
+    if (users) {
+      users.push(userData);
+    } else {
+      users = [userData];
+    }
+
+    localStorage.setItem('users', JSON.stringify(users));
+    alert('Registration successful!');
+    document.getElementById('register-Form').reset();
+    return true;
+  } else {
+    alert('Passwords do not match. Please try again.');
+    return false;
+  }
+}
